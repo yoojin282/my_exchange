5b662a1b645a4d0919e9d604af36da307fe68b4a
In-gi, Park <yoojin282@gmai.com>
2024-05-25 08:35:50 +0900

    vscode 설정파일 git에서 제거

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
-const String apiKey = 'F4FQRaV47zxbP6l86JiOXnV0HYT5PVAB';
+const String apiKey = String.fromEnvironment('API_KEY');
+// 'F4FQRaV47zxbP6l86JiOXnV0HYT5PVAB'
 final Uri apiUrl = Uri.parse(
     'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON');
 const List<String> availableUnits = ['THB', 'USD', "JPY(100)"];
