9246cf4610a9d5c68a09d944e9bd80295bc10bd3
In-gi, Park <yoojin282@gmai.com>
2024-06-26 20:25:01 +0900

    불필요 https 관련 도메인 체크로직 제거

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:my_exchange/get_it.dart';
 import 'package:my_exchange/screen/home_page.dart';
@@ -7,11 +5,11 @@ import 'package:my_exchange/theme.dart';
 
 Future<void> main() async {
   initializeGetIt();
-  WidgetsFlutterBinding.ensureInitialized();
+  // WidgetsFlutterBinding.ensureInitialized();
   // final data = await PlatformAssetBundle().load('certs/lets-encrypt-r3.pem');
   // SecurityContext.defaultContext
   //     .setTrustedCertificatesBytes(data.buffer.asUint8List());
-  HttpOverrides.global = NoCheckCerfiticationHttpOverrides();
+  // HttpOverrides.global = NoCheckCerfiticationHttpOverrides();
   runApp(const MyApp());
 }
 
@@ -37,19 +35,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class NoCheckCerfiticationHttpOverrides extends HttpOverrides {
-  @override
-  HttpClient createHttpClient(SecurityContext? context) {
-    return super.createHttpClient(context)
-      ..badCertificateCallback = (X509Certificate cert, String host, int port) {
-        // Allowing only our Base API URL.
-        List<String> validHosts = ["www.koreaexim.go.kr"];
+// class NoCheckCerfiticationHttpOverrides extends HttpOverrides {
+//   @override
+//   HttpClient createHttpClient(SecurityContext? context) {
+//     return super.createHttpClient(context)
+//       ..badCertificateCallback = (X509Certificate cert, String host, int port) {
+//         // Allowing only our Base API URL.
+//         List<String> validHosts = ["api.apilayer.com"];
 
-        final isValidHost = validHosts.contains(host);
-        return isValidHost;
+//         final isValidHost = validHosts.contains(host);
+//         return isValidHost;
 
-        // return true if you want to allow all host. (This isn't recommended.)
-        // return true;
-      };
-  }
-}
+//         // return true if you want to allow all host. (This isn't recommended.)
+//         // return true;
+//       };
+//   }
+// }
