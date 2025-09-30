fe252f939f7b04150a6f1cfa7d2088f1119ebf44
In-gi, Park <yoojin282@gmail.com>
2025-09-30 19:43:34 +0900

    chore: 라이브러리 최신화 feat: 베트남 동 추가

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,8 +3,8 @@ import 'package:logger/logger.dart';
 
 const apiHost = 'api.apilayer.com';
 const baseUnit = 'KRW';
-const availableUnits = ['THB', 'USD'];
-const availableLanguage = ['KO', 'TH', "EN"];
+const availableUnits = ['THB', 'VND', 'USD'];
+const availableLanguage = ['KO', 'TH', "EN", 'VN'];
 const shortcuts = [20, 100, 500, 1000, 5000];
 const reverseShortcuts = [1000, 5000, 10000, 50000, 100000];
 final dateFormat = DateFormat('yyyy-MM-dd');
